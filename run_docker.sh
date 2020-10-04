3bca0306a44a00cc9c825b0fe8948073ab4b08b9
Jianxiong Cai <jicai@seas.upenn.edu>
2020-10-04 13:43:11 -0400

    [DONE] fixed typo

--- run_docker.sh
+++ run_docker.sh
@@ -6,4 +6,4 @@
 # /workspace/data: the data folder
 # /workspace/code: the code folder
 
-docker run -it -p 8890:8888 -p 6008:6006 --gpus all -v /home/jianxiong/data/RobotFly:/workspace/data -v /home/jianxiong/Desktop/CIS680-HW3:/workspace/CIS680-HW3 jianxiongcai/robotfly-pytorch
+docker run -it -p 8890:8888 -p 6008:6006 --gpus all -v /home/jianxiong/data/HW3:/workspace/data -v /home/jianxiong/Desktop/CIS680-HW3:/workspace/CIS680-HW3 jianxiongcai/robotfly-pytorch
