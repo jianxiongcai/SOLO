5cdec72b86647af941538e288daa67ec5d73f925
Jianxiong Cai <jicai@seas.upenn.edu>
2020-10-04 13:35:19 -0400

    add docker script

---
+++ run_docker.sh
@@ -0,0 +1,9 @@
+# Networking:
+# bind port: host (8890) => jupyter lab (8888)
+# bind port: host (6008) => tensorboard (6006)
+
+# Directionary:
+# /workspace/data: the data folder
+# /workspace/code: the code folder
+
+docker run -it -p 8890:8888 -p 6008:6006 --gpus all -v /home/jianxiong/data/RobotFly:/workspace/data -v /home/jianxiong/Desktop/CIS680-HW3:/workspace/code jianxiongcai/robotfly-pytorch
